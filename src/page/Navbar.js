49048595eab826d403e93edbbb1fa878c3577101
AkshayPatil07695 <146464776+AkshayPatil076@users.noreply.github.com>
2024-04-03 12:20:52 +0530

    Update Navbar.js

--- src/page/Navbar.js
+++ src/page/Navbar.js
@@ -3,9 +3,9 @@ import './assite/navbar.css'
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-let b ;
 // Change the language of an website text 
-export function language(b){
+export function language(){
+   let b ='hindi';
    const English = ["Milk Collection & Billing","Reports","Setting ","Client","Contact"]
    const Hindi = [" दूध खरेदी आणि विक्री","रिपोर्टों","सेटिंग","ग्राहक","संपर्क"];
    // const list = Hindi.map((Hindi)=> <p>{Hindi}</p>)
@@ -30,52 +30,51 @@ function Navbar(){
 
       <>
       
-   {language("hindi")}
        {/* <!--=============== HEADER ===============--> */}
-      <header class="header">
-         <nav class="nav container">
-            <div class="nav__data">
-               <a href="#" class="nav__logo" >
-               <i class="bi bi-bookmark-fill"></i> &nbsp; &nbsp; &nbsp; Company
+      <header className="header">
+         <nav className="nav container">
+            <div className="nav__data">
+               <a href="#" className="nav__logo" >
+               <i className="bi bi-bookmark-fill"></i> &nbsp; &nbsp; &nbsp; Company
                </a>
                
-               <div class="nav__toggle" id="nav-toggle">
-                  {/* <i class="ri-menu-line nav__burger"></i> */} <i class="bi bi-menu-button-wide-fill  nav__burger"></i>
-                  {/* <i class="ri-close-line nav__close"></i> */} <i class="bi bi-menu-button-fill nav__burger"></i>
+               <div className="nav__toggle" id="nav-toggle">
+                  {/* <i classname="ri-menu-line nav__burger"></i> */} <i className="bi bi-menu-button-wide-fill  nav__burger"></i>
+                  {/* <i classname="ri-close-line nav__close"></i> */} <i className="bi bi-menu-button-fill nav__burger"></i>
                </div>
                
             </div>
 
             {/* <!--=============== NAV MENU ===============--> */}
-            <div class="nav__menu" id="nav-menu">
-               <ul class="nav__list">
-                  <li><a href="#" class="nav__link"><i class="bi bi-pc-display-horizontal"></i>  &nbsp;&nbsp;&nbsp;{language()[0]}</a></li>
+            <div className="nav__menu" id="nav-menu">
+               <ul className="nav__list">
+                  <li><a href="#" className="nav__link"><i className="bi bi-pc-display-horizontal"></i>  &nbsp;&nbsp;&nbsp;{language()[0]}</a></li>
                   {/* दूध खरेदी आणि विक्री */}
 
       {/* reports */}
                   {/* <li><a href="#" class="nav__link"><i class="bi bi-flag-fill"></i>&nbsp;&nbsp;&nbsp;रिपोर्टों</a></li> */}
 
                   {/* <!--=============== DROPDOWN 1 ===============--> */}
-                  <li class="dropdown__item">
-                     <div class="nav__link"><i class="bi bi-flag-fill"></i>&nbsp;&nbsp;&nbsp;    
-                     {language()[1]} <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                  <li className="dropdown__item">
+                     <div className="nav__link"><i className="bi bi-flag-fill"></i>&nbsp;&nbsp;&nbsp;    
+                     {language()[1]} <i className="ri-arrow-down-s-line dropdown__arrow"></i>
                      </div>
 
-                     <ul class="dropdown__menu">
+                     <ul className="dropdown__menu">
                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-pie-chart-line"></i> Overview
+                           <a href="#" className="dropdown__link">
+                              <i clas="ri-pie-chart-line"></i> Overview
                            </a>                          
                         </li>
 
                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-arrow-up-down-line"></i> Transactions
+                           <a href="#" className="dropdown__link">
+                              <i className="ri-arrow-up-down-line"></i> Transactions
                            </a>
                         </li>
                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-arrow-up-down-line"></i> Transactions
+                           <a href="#" className="dropdown__link">
+                              <i className="ri-arrow-up-down-line"></i> Transactions
                            </a>
                         </li>
 
@@ -84,36 +83,36 @@ function Navbar(){
                      </ul>
                   </li>
                   
-                  <li><a href="#" class="nav__link"><i class="bi bi-gear-wide-connected"></i>&nbsp;&nbsp;&nbsp;{language()[2]}</a></li>
+                  <li><a href="#" className="nav__link"><i className="bi bi-gear-wide-connected"></i>&nbsp;&nbsp;&nbsp;{language()[2]}</a></li>
 
                   {/* <!--=============== DROPDOWN 2 ===============--> */}
-                  <li class="dropdown__item">
-                     <div class="nav__link">
-                     <i class="bi bi-people-fill"></i>&nbsp;&nbsp;&nbsp;{language()[3]}
+                  <li className="dropdown__item">
+                     <div className="nav__link">
+                     <i className="bi bi-people-fill"></i>&nbsp;&nbsp;&nbsp;{language()[3]}
                      </div>
 
-                     <ul class="dropdown__menu">
+                     <ul className="dropdown__menu">
                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-user-line"></i> Profiles
+                           <a href="#" className="dropdown__link">
+                              <i className="ri-user-line"></i> Profiles
                            </a>                          
                         </li>
 
                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-lock-line"></i> Accounts
+                           <a href="#" className="dropdown__link">
+                              <i className="ri-lock-line"></i> Accounts
                            </a>
                         </li>
 
                         <li>
-                           <a href="#" class="dropdown__link">
-                              <i class="ri-message-3-line"></i> Messages
+                           <a href="#" className="dropdown__link">
+                              <i className="ri-message-3-line"></i> Messages
                            </a>
                         </li>
                      </ul>
                   </li>
 
-                  <li><a href="#" class="nav__link"><i class="bi bi-telephone-fill"></i>&nbsp;&nbsp;&nbsp;{language()[4]}</a></li>
+                  <li><a href="#" className="nav__link"><i className="bi bi-telephone-fill"></i>&nbsp;&nbsp;&nbsp;{language()[4]}</a></li>
                </ul>
             </div>
          </nav>
