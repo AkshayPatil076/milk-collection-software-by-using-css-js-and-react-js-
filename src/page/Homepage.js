63e2f95a4bdfde84ce373c13695987504190efd7
AkshayPatil07695 <146464776+AkshayPatil076@users.noreply.github.com>
2024-04-04 16:10:33 +0530

    Update Homepage.js
    in this i can add the code for show to curent live date  output to the user .

--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -3,30 +3,53 @@ import Navbar from './Navbar'
 import './assite/Homepage.css'
 import Billingpage from './Billingpage'
 import Daryfarm from './img/vecteezy_dairy-farm-label-logo-with-a-dairy-cow-cartoon_7144859.jpg'
+import  { useState } from 'react';
+
+
+import { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+
+
 
 export default function Homepage() {
-  
+
+  const [currentDate, setCurrentDate] = useState(getFormattedDate(new Date()));
+
+  // Function to get the formatted date (YYYY-MM-DD)
+  function getFormattedDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
+
   return (
    <>
     <Navbar />
    
-    <div  id='datebutton'>
+    <div  id='datebutton '>
 
       <div className='form'>
-      <input type='date' className='date' />
+      <input    id="dateInput"
+        type="date"
+        value={currentDate}
+        onChange={(e) => setCurrentDate(e.target.value)} 
+        className='date' />
       <br /><br /><br />
-      <button className='click' onClick={
-        function clickvalue (){
-          document.getElementById('enter').style.display='block';
-          document.getElementById('datebutton').style.display='none';
-        }
-      }>START</button>
+      
+      <button className='click' to='/'>START</button>
       </div>
       <img className='daryimage' src={Daryfarm} />
 
     </div>
 
-      {/* <Billingpage /> */}
+
    </>
     
   )
